7b0d1acf58ff199bbc5cd99e753920a98e49d0f9
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-04-12 18:27:31 +0200

    Install PostCSS, TailwindCSS

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'tailwindcss/tailwind.css';
 
 ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,7 +4,7 @@ module.exports = {
     rules: [
       {
         test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
