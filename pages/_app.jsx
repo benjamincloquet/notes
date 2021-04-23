659d12d91d040f02464ce19a3e649e9ec7f161d9
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2021-04-23 18:01:16 +0200

    Fix css import

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-props-no-spreading */
-import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
